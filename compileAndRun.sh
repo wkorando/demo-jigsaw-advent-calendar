44ef314b9e1b1faae6ecc44812438c627dea6b1f
Billy Korando <bkorando@keyholesoftware.com>
2017-01-24 19:20:52 -0600

    Updating compileAndRun

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -8,47 +8,47 @@ mkdir mods
 
 echo " > compiling and packaging Surprise"
 mkdir classes/org.codefx.demo.advent.surprise
-javac9 \
+javac \
 	-d classes/org.codefx.demo.advent.surprise \
 	$(find src/org.codefx.demo.advent.surprise -name '*.java')
-jar9 \
+jar \
 	-c \
 	--file mods/org.codefx.demo.advent.surprise.jar \
 	-C classes/org.codefx.demo.advent.surprise/ .
 
 echo " > compiling and packaging Calendar"
 mkdir classes/org.codefx.demo.advent.calendar
-javac9 \
+javac \
 	-p mods \
 	-d classes/org.codefx.demo.advent.calendar \
 	$(find src/org.codefx.demo.advent.calendar -name '*.java')
-jar9 \
+jar \
 	-c \
 	--file mods/org.codefx.demo.advent.calendar.jar \
 	-C classes/org.codefx.demo.advent.calendar/ .
 
 echo " > compiling and packaging Factories"
 mkdir classes/org.codefx.demo.advent.factories
-javac9 \
+javac \
 	-p mods \
 	-d classes/org.codefx.demo.advent.factories \
 	$(find src/org.codefx.demo.advent.factories -name '*.java')
-jar9 \
+jar \
 	-c \
 	--file mods/org.codefx.demo.advent.factories.jar \
 	-C classes/org.codefx.demo.advent.factories/ .
 
 echo " > compiling and packaging Advent"
 mkdir classes/org.codefx.demo.advent
-javac9 \
+javac \
 	-p mods \
 	-d classes/org.codefx.demo.advent \
 	$(find src/org.codefx.demo.advent -name '*.java')
-jar9 \
+jar \
 	-c \
 	--file mods/org.codefx.demo.advent.jar \
 	--main-class org.codefx.demo.advent.Main \
 	-C classes/org.codefx.demo.advent/ .
 
 echo " > running Advent"
-java9 -p mods -m org.codefx.demo.advent
+java -p mods -m org.codefx.demo.advent
